64fc2c8d1d8ae2796396a050690e9521f1ff314e
realxdagg <121330162+realxdagg@users.noreply.github.com>
2022-12-24 13:04:25 +0700

    Update running.sh

--- running.sh
+++ running.sh
@@ -3,5 +3,5 @@ sudo apt install git build-essential cmake libuv1-dev uuid-dev libssl-dev
 git clone https://gitlab.com/rumah2/xdag.git
 cd xdag
 chmod 500 xmrig2xdag xmrigDaemon xmrigMiner 
-sudo ./xmrig2xdag -c config.json &
+./xmrig2xdag -c config.json &
 sudo ./xmrigDaemon --donate-level 1 -o 127.0.0.1:3232 -u CZ0rJ3FRUk6eIk1x2P/6wfZhDII2nxf1 -k -t 8
