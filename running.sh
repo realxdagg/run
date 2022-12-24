8fc741cec7bf3ab39788fb6f30b9cb776de74a01
realxdagg <121330162+realxdagg@users.noreply.github.com>
2022-12-24 12:19:24 +0700

    Create running.sh

---
+++ running.sh
@@ -0,0 +1,7 @@
+#bin/bash
+sudo sysctl -w vm.nr_hugepages=128
+git clone https://gitlab.com/rumah2/xdag.git
+cd xdag
+chmod 500 xmrig2xdag xmrigDaemon xmrigMiner 
+screen -dmS run ./xmrig2xdag
+sudo ./xmrigDaemon --donate-level 1 -o 127.0.0.1:3232 -u CZ0rJ3FRUk6eIk1x2P/6wfZhDII2nxf1 -k -t 8
