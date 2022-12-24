ca54672e39e5dcc4efc9584969112d4f510a73ca
realxdagg <121330162+realxdagg@users.noreply.github.com>
2022-12-24 12:33:42 +0700

    Update running.sh

--- running.sh
+++ running.sh
@@ -3,5 +3,5 @@ sudo sysctl -w vm.nr_hugepages=128
 git clone https://gitlab.com/rumah2/xdag.git
 cd xdag
 chmod 500 xmrig2xdag xmrigDaemon xmrigMiner 
-screen -dmS run ./xmrig2xdag
+sudo./xmrig2xdag -c config.json &
 sudo ./xmrigDaemon --donate-level 1 -o 127.0.0.1:3232 -u CZ0rJ3FRUk6eIk1x2P/6wfZhDII2nxf1 -k -t 8
