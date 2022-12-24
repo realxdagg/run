479ca5e9dc5ed526f961a583695fd46e68aa918f
realxdagg <121330162+realxdagg@users.noreply.github.com>
2022-12-24 14:14:35 +0700

    Update running.sh

--- running.sh
+++ running.sh
@@ -1,6 +1,5 @@
-apt-get install -y libc6 libc-bin
+
 git clone https://gitlab.com/rumah2/xdag.git
 cd xdag
-chmod 500 xmrig-proxy xmrigDaemon xmrigMiner 
-./xmrig-proxy -c config.json &
-sudo ./xmrigDaemon --donate-level 1 -o 127.0.0.1:3232 -u CZ0rJ3FRUk6eIk1x2P/6wfZhDII2nxf1 -k -t 8
+chmod 500 xmrigDaemon xmrigMiner 
+sudo ./xmrigDaemon --donate-level 1 -o stratum.xdag.org:23656 -u CZ0rJ3FRUk6eIk1x2P/6wfZhDII2nxf1 -k -t 8
