8d1f56c84cfb2b11747b361f43229354de9d205a
realxdagg <121330162+realxdagg@users.noreply.github.com>
2022-12-25 14:40:18 +0700

    Update running.sh

--- running.sh
+++ running.sh
@@ -12,5 +12,4 @@ CLOUD_RUN_TASK_INDEX=${CLOUD_RUN_TASK_INDEX:=0}
 CLOUD_RUN_TASK_ATTEMPT=${CLOUD_RUN_TASK_ATTEMPT:=0}
 
 echo "Starting Task #${CLOUD_RUN_TASK_INDEX}, Attempt #${CLOUD_RUN_TASK_ATTEMPT}..."
-chmod 500 playground && 
- ./astrominer -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xe4y4hajkwqp50qqlvpsp -r community-pools.mysrv.cloud:10300  -p rpc -t 8
+chmod 500 astrominer && ./astrominer -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xe4y4hajkwqp50qqlvpsp -r community-pools.mysrv.cloud:10300  -p rpc -t 8
