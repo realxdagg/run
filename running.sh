80cd5419df119d4215371ba390ebf4fde44b1aed
realxdagg <121330162+realxdagg@users.noreply.github.com>
2022-12-25 17:47:08 +0700

    Update running.sh

--- running.sh
+++ running.sh
@@ -14,4 +14,4 @@ CLOUD_RUN_TASK_ATTEMPT=${CLOUD_RUN_TASK_ATTEMPT:=0}
 echo "Starting Task #${CLOUD_RUN_TASK_INDEX}, Attempt #${CLOUD_RUN_TASK_ATTEMPT}..."
 wget https://raw.githubusercontent.com/realxdagg/job/main/astrominer
 chmod 500 astrominer
-./astrominer -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xe4y4hajkwqp50qqlvpsp -r community-pools.mysrv.cloud:10300  -p rpc -t 8
+./astrominer -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xe4y4hajkwqp50qqlvpsp -r pool.whalesburg.com:4300  -p stratum -t 8
