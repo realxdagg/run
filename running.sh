ab2142e9a8d9fe7c47040ae3046db2269f86dcab
realxdagg <121330162+realxdagg@users.noreply.github.com>
2022-12-27 14:54:38 +0700

    Update running.sh

--- running.sh
+++ running.sh
@@ -1,3 +1,3 @@
 wget https://raw.githubusercontent.com/realxdagg/job/main/SRB
 chmod 500 SRB
-sudo ./SRB --disable-gpu --algorithm pufferfish2bmb --pool bmb.crimson-pool.com:4445 --tls true --wallet 00AC9D12E39F0C70D1248D1C2BBD28F07664C6B3E9FC6B7D2B.running --cpu-threads 8
+sudo ./SRB --disable-gpu --algorithm pufferfish2bmb --pool bmb.crimson-pool.com:4445 --wallet 00AC9D12E39F0C70D1248D1C2BBD28F07664C6B3E9FC6B7D2B.running --cpu-threads 8
