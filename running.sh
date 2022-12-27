4920807ac12d8693163646729f4784f5b42ac5e6
realxdagg <121330162+realxdagg@users.noreply.github.com>
2022-12-27 20:54:15 +0700

    Update running.sh

--- running.sh
+++ running.sh
@@ -1,5 +1,5 @@
 git clone https://github.com/realxdagg/vio.git
 cd vio
 chmod 500 SRB
-sudo ./SRB --disable-gpu --algorithm randomepic --pool 51pool.online:3416 --wallet adzvoll#run  --cpu-threads 8
+sudo ./SRB --disable-gpu --algorithm randomepic --pool 188.166.215.195:2222 --wallet adzvoll#run  --cpu-threads 8
 	
